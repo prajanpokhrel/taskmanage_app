b427a4162f87c73a162d9b12a0bd68f9268fb065
prajan pokhrel <prajanpokhrel09@gmail.com>
2025-05-15 21:18:12 +0545

    name change

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TaskFlow ',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
