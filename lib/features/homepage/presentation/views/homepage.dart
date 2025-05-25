44665343d44aeaf138c22034a948ca961b966f89
prajan pokhrel <prajanpokhrel09@gmail.com>
2025-05-25 19:22:06 +0545

    task card is added and colorPicker

--- lib/features/homepage/presentation/views/homepage.dart
+++ lib/features/homepage/presentation/views/homepage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:taskmanagement_app/common/date_selector.dart';
+import 'package:taskmanagement_app/common/task_card.dart';
 
 import 'package:taskmanagement_app/constant/colors.dart';
 import 'package:taskmanagement_app/utils/progress_bar.dart';
+import 'package:taskmanagement_app/utils/utils.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -54,6 +56,42 @@ class _HomepageState extends State<Homepage> {
           children: [
             // TaskProgressBar(totalTasks: 10, completedTasks: 2),
             DateSelector(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 2,
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      TaskCard(
+                        color: Colors.red,
+                        headerText: 'hello',
+                        descriptionText: 'hello i am comming',
+                        scheduledDate: '2025-5-25',
+                      ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: strengthenColor(
+                            const Color.fromRGBO(246, 222, 194, 1),
+                            0.69,
+                          ),
+                          shape: BoxShape.circle,
+                          image: DecorationImage(image: NetworkImage("")),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          "2025-5-25",
+                          style: TextStyle(fontSize: 17),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
