84a75e10be5e4a0dddcf6d296d7b9a974401e016
prajan pokhrel <prajanpokhrel09@gmail.com>
2025-06-02 21:27:39 +0545

    firebase is initilized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core_dart/firebase_core_dart.dart';
 import 'package:flutter/material.dart';
 import 'package:taskmanagement_app/app/app.dart';
+import 'package:taskmanagement_app/firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
